40edbd69a75ffb7ba0136b77f0c0b37dc9361566
TimRCAnderson <timothyrcanderson@gmail.com>
2017-04-22 08:32:48 -0400

    Initial functionality. Needs testing and possible refinement.

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log("This is loaded.");
+
+exports.twitterkeys = {
+  consumer_key: "SYhviZkXr4CLd7MiY8BPGZnUb",
+  consumer_secret: "NEo4WiLBNGOOtudUjFeou7aiAMCPvHCzQkHYRXJVoN9RTVKgnN",
+  access_token_key: "2506377454-ALNTwpCr9Kr7ZboEDDkJmgx1L80CrtMkKtn1GQV",
+  access_token_secret: "6V6Aw7cdcJ0LJwjgAPVK8ibv7cltqQrJP3zvDJXqdRpni"
+}
---
+++ liri.js
@@ -0,0 +1,140 @@
+var keys = require("./keys");
+var fs = require("fs");
+var twitter = require("twitter");
+var spotify = require("spotify");
+var request = require("request");
+
+var client = new twitter(keys.twitterkeys);
+
+function LIRI(method, param)
+{
+  switch(method)
+  {
+    case "my-tweets":
+      //show your last 20 tweets and when they were created at in your terminal/bash window.
+      var params = {screen_name: "trcanderson"};
+      client.get("statuses/user_timeline", params, function(error, tweets, response) {
+        if(!error)
+        {
+          var appendString = ``;
+          for(var i = 0; i < 20; i++)
+          {
+            appendString += `${tweets[i].created_at}: ${tweets[i].text}
+`;
+          }
+          fs.appendFile("log.txt", appendString, function(error) {
+            if(error)
+            {
+              console.log(error);
+            }
+            else
+            {
+              console.log(appendString);
+            }
+          });
+        }
+        else
+        {
+          console.log(error);
+        }
+      });
+      break;
+    case "spotify-this-song":
+      //show the following information about the song in your terminal/bash window:
+      //Artist(s), The song's name, A preview link of the song from Spotify, The album that the song is from
+      //default: "The Sign", Ace of Base
+      spotify.search({type: "track", query: (param || "The Sign Ace of Base")}, function(error, data) {
+        if(error)
+        {
+          console.log(error);
+        }
+        else
+        {
+          var artistsList = "";
+          for(var i = 0; i < data.tracks.items[1].artists.length; i++)
+          {
+            artistsList += data.tracks.items[1].artists[i].name;
+            if(i < data.tracks.items[1].artists.length - 1)
+            {
+              artistsList += ", "
+            }
+          }
+          var songInfo = {
+            artists: artistsList,
+            song: data.tracks.items[1].name,
+            link: data.tracks.items[1].preview_url,
+            album: data.tracks.items[1].album.name
+          }
+          var appendString = `Artists: ${songInfo.artists}
+Song: ${songInfo.song}
+Link: ${songInfo.link}
+Album: ${songInfo.album}
+`;
+          fs.appendFile("log.txt", appendString, function(error) {
+            if(error)
+            {
+              console.log(error);
+            }
+            else
+            {
+              console.log(appendString);
+            }
+          });
+        }
+      });
+      break;
+    case "movie-this":
+      //output the following information to your terminal/bash window:
+      //Title of the movie, Year the movie came out, IMDB Rating of the movie, Country where the movie was produced, Language of the movie, Plot of the movie, Actors in the movie, Rotten Tomatoes Rating, Rotten Tomatoes URL.
+      //default: Mr. Nobody
+      var title = param || "Mr Nobody";
+      console.log(title);
+      var queryURL = "http://www.omdbapi.com/?t=" + title.trim().split(" ").join("+") + "&plot=short&r=json";
+      request(queryURL, function(error, response, body) {
+        if(!error)
+        {
+          var ratingLookup = {};
+          for (var i = 0, len = array.length; i < len; i++)
+          {
+            ratingLookup[body.Ratings[i].source] = body.Ratings[i];
+          }
+          var appendString = `Title: ${body.Title}
+Year: ${body.Year}
+IMDB Rating: ${ratingLookup["Internet Movie Database"].value}
+Country: ${body.Country}
+Language: ${body.Language}
+Plot: ${body.Plot}
+Actors: ${body.Actors}
+Rotten Tomatoes Rating: ${ratingLookup["Rotten Tomatoes"]}
+`
+          fs.appendFile("log.txt", appendString, function(error) {
+            if(!error)
+            {
+              console.log(appendString);
+            }
+            else
+            {
+              console.log(error);
+            }
+          });
+        }
+        else
+        {
+          console.log(error);
+        }
+      });
+      break;
+    case "do-what-it-says":
+      //LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
+      fs.readFile("random.txt", "utf8", function(error, data) {
+        if(!error)
+        {
+          var args = data.split(",");
+          args[1] = args[1].substring(1, (args[1].length - 1));
+          LIRI(args[0], args[1]);
+        }
+      });
+  }
+}
+
+LIRI(process.argv[2], process.argv[3]);
